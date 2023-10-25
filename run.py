e68361e309d13996dcb8373e782774ca96742c02
Nicolas Nassar <nicolasnassar.contac@gmail.com>
2023-10-25 17:38:53 -0300

    removendo comententarios do arquivo car class

--- run.py
+++ run.py
@@ -1,12 +1,12 @@
 from car import Car
 from electric_car import ElectricCar
 
-# car1 = Car("Ka","Ford", 
-#            "Black", "2021")
-# print(car1.mileage)
-# car1.start()
-# car1.drive(3)
-# print(car1.mileage)
+car1 = Car("Ka","Ford", 
+           "Black", "2021")
+print(car1.mileage)
+car1.start()
+car1.drive(3)
+print(car1.mileage)
 
 ele = ElectricCar("Suv","BYD", "Red", "2023", 1.5)
 # print(ele.mileage)
